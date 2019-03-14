d597406f2a40174835bdc573647b8d069c862198
hansow <nachoagus@live.com.ar>
2019-03-14 11:56:37 -0300

    Add files via upload

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+    SceneLoader scene_;
+    public string scene;
+    
+    void Start()
+    {
+        scene_.GetComponent<SceneLoader>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            scene_.LoadSceneAsync(scene);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            scene_.LoadSceneAsync(scene);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            scene_.LoadSceneAsync(scene);
+        }
+    }
+}
---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader instance = null;
+    AsyncOperation asyncLoader;
+
+    public void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void LoadScenes(string scene)
+    {
+        StartCoroutine(LoadSceneAsync(scene));
+    }
+
+    public IEnumerator LoadSceneAsync(string scene)
+    {
+        yield return new WaitForSeconds(3f);
+        asyncLoader = SceneManager.LoadSceneAsync(scene);
+        while(!asyncLoader.isDone)
+        {
+            Debug.Log("Loading Progress: " + asyncLoader.progress);
+            yield return null;
+        }
+    }
+}
+
